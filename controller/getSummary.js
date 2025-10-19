f670d9e1e6be80ba060236b4ba9635a964066444
LeskarX <leskarzone123@gmail.com>
2025-10-19 22:06:54 +0530

    use new model

--- controller/getSummary.js
+++ controller/getSummary.js
@@ -1,7 +1,9 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-const apiKey=process.env.API.toString()
-const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+// import { GoogleGenerativeAI } from "@google/generative-ai"
+import { GoogleGenAI } from "@google/genai";
+// const apiKey=process.env.API.toString()
+
+const genAI = new GoogleGenAI({});
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 async function getSummary(req,res){
@@ -9,11 +11,16 @@ async function getSummary(req,res){
     const {text}=req.body
     // console.log(text)
     const prompt = `summarize the given data and answer in one single paragraph.if the data is in different languages, in that case also give the result in english follow it strictly only english data:${text}`;
-    const result = await model.generateContent(prompt);
+    // const result = await model.generateContent(prompt);
+    const response = await genAI.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+    // console.log("----------->",response.text);
 // console.log(result.response.text());
     // const summary=await getSummary(text)
     res.json({
-      "message":result.response.text(),
+      "message":response.text,
     })
    
 
